486dc0f6797541e40fb0da03ca7d3216a182cad4
Bengt Brodersen <bengt.brodersen@gmail.com>
2019-09-25 11:51:08 +0200

    refactor: get rid of new  formating feature to be compatible with python 3.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read_text(file_name):
 
 setuptools.setup(
     name='aws-configure',
-    version='2.1.1',
+    version='2.1.2',
     author="Bengt Brodersen",
     author_email="me@qoomon.me",
     description="A CLI to configure AWS named profiles in ~/.aws/config and ~/.aws/credentials files",
